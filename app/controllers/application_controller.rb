64b2f3ddc5243e3813e79b73074aa3ef4193b0a0
kawasaki1207 <ren_prpr@icloud.com>
2022-05-10 03:37:38 +0900

    BASIC認証変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      # username == "test1207" && password == "1207"
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
