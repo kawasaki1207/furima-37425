3c241307d88e51981684ef3745b79a1d37b577ca
kawasaki1207 <ren_prpr@icloud.com>
2022-05-10 03:18:59 +0900

    デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   def basic_auth
-    authenticate_or_repuest_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "test1207" && password == "1207"
+      # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
