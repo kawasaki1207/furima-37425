7a39ecda0ea6ab31d3ceddd3980ae429b2cc9602
kawasaki1207 <ren_prpr@icloud.com>
2022-05-19 22:29:54 +0900

    アクティブハッシュの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shopping_day
+
+  validates :neme, presence: true
+  validates :content, presence: true
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id, 
+    validates :status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :shopping_day_id
+  end
+
+  validates :price, presence: true
 end
