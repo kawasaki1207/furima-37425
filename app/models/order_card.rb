92476c5af414cdd2ddb1be42d840ee57228ef3aa
kawasaki1207 <ren_prpr@icloud.com>
2022-06-12 03:43:41 +0900

    モデルテスト

--- app/models/order_card.rb
+++ app/models/order_card.rb
@@ -6,7 +6,7 @@ class OrderCard
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
-    validates :tel, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :tel, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :user_id
     validates :item_id
---
+++ spec/models/order_card_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe OrderCard, type: :model do
+  before do
+    @order_card = FactoryBot.build(:order_card)
+  end
+
+  describe '商品購入' do
+    context '商品購入できる場合' do
+      it '全ての項目に値が存在すれば購入できる' do
+        expect(@order_card).to be_valid
+      end
+      it 'buildingが空でも購入できる' do
+        @order_card.building = ""
+        expect(@order_card).to be_valid
+      end
+    end
+
+    context '商品購入できない場合' do
+      it 'post_codeが空だと購入できない' do
+        @order_card.post_code = ""
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Post code can't be blank"
+      end
+      it 'cityが空だと購入できない' do
+        @order_card.city = ""
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "City can't be blank"
+      end
+      it 'addressが空だと購入できない' do
+        @order_card.address = ""
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Address can't be blank"
+      end
+      it 'telが空だと購入できない' do
+        @order_card.tel = ""
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Tel can't be blank"
+      end
+      it 'prefecture_idが空だと購入できない' do
+        @order_card.prefecture_id = 0
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it 'userが紐付いていないと購入できない' do
+        @order_card.user_id = nil
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "User can't be blank"
+      end
+      it 'itemが紐付いていないと購入できない' do
+        @order_card.item_id = nil
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Item can't be blank"
+      end
+      it 'tokenが空だと購入できない' do
+        @order_card.token = ""
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Token can't be blank"
+      end
+      it 'post_cordにハイフンがないと購入できない' do
+        @order_card.post_code = "1234567"
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Post code is invalid. Include hyphen(-)"
+      end
+      it 'telにハイフンがあると購入できない' do
+        @order_card.tel = "123-1234-1234"
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Tel is invalid"
+      end
+      it 'telが10桁以下だと購入できない' do
+        @order_card.tel = "123456789"
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Tel is invalid"
+      end
+      it 'telが11桁以上だと購入できない' do
+        @order_card.tel = "123456789100"
+        @order_card.valid?
+        expect(@order_card.errors.full_messages).to include "Tel is invalid"
+      end
+    end
+    
+  end
+end
